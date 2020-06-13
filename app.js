d23a760b266f865db77b60b03491ef31472724cd
Nevets08 <Nevets08@users.noreply.github.com>
2020-06-13 23:05:32 +0200

    Move the div with mouse and ball in development

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const bar = document.getElementById('bar');
+const ball = document.querySelector('.ball');
 
 const number = document.getElementById('number');
-let numberValue = 0;
+let i = 0;
+// Move the #bar div with the mouse (only x)
+window.addEventListener('mousemove', (e) => {
+    number.innerHTML = e.x + ' px';
+    bar.parentElement.classList.remove('container-bar')
 
-window.addEventListener('keydown', () => {
-    if (event.keyCode === 39) {
-        numberValue += 30;
-        number.innerHTML = numberValue + ' px';
-        bar.style.transform = 'translateX(' + numberValue + 'px)';
+    if (e.x > screen.width - 400 || e.x < 200) {
+        return;
+    } else {
+        bar.style.transform = 'translateX(' + e.x + 'px)';
     }
-    
-    if (event.keyCode === 37) {
-        numberValue -= 30;
-        number.innerHTML = numberValue + ' px';
-        bar.style.transform = 'translateX(' + numberValue + 'px)';
+});
+
+// Move the ball up and down
+setInterval(() => {
+    if (i === 250) {
+        i--;
+        ball.style.transform = 'translateY(' + i + 'px)';
+    } else {
+        i++;
+        ball.style.transform = 'translateY(' + i + 'px)';
     }
-    
-    console.log(bar)
-}); 
+    console.log(i)
+}, 10);
+
+
+// console.log(bar.getBoundingClientRect().top)
